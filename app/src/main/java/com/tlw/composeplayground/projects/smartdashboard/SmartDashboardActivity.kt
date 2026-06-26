4a5984ffb24f90e883703a001234a855c99023a9
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2026-06-26 13:42:29 +0530

    implement a basic sign up screen UI in SmartDashboardActivity

--- app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
+++ app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
@@ -4,9 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.tlw.composeplayground.projects.smartdashboard.ui.theme.ComposePlaygroundTheme
 
 class SmartDashboardActivity : ComponentActivity() {
@@ -15,8 +32,51 @@ class SmartDashboardActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposePlaygroundTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color.Black,
+                                        Color(0xFF333333)
+                                    )
+                                )
+                            )
+                            .padding(innerPadding)
 
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .size(40.dp)
+                                    .clip(CircleShape)
+                                    .background(
+                                        Color(0xFF273233)
+                                    ),
+                                imageVector = Icons.AutoMirrored.Rounded.KeyboardArrowLeft,
+                                contentDescription = null,
+                                tint = Color.White
+                            )
+                            Spacer(modifier = Modifier.height(20.dp))
+                            Text(
+                                "Sign Up",
+                                color = Color.White,
+                                style = MaterialTheme.typography.headlineLarge
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                "New here? Let's get started on your smart home journey in just few simple steps",
+                                color = Color.White,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    }
                 }
             }
         }
