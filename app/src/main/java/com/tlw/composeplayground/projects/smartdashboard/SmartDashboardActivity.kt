64275b3b625e2561ad0e9005919db3017ec44647
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2026-06-26 11:14:10 +0530

    add SmartDashboardActivity and basic UI theme infrastructure

---
+++ app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
@@ -0,0 +1,24 @@
+package com.tlw.composeplayground.projects.smartdashboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.tlw.composeplayground.projects.smartdashboard.ui.theme.ComposePlaygroundTheme
+
+class SmartDashboardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposePlaygroundTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+
+                }
+            }
+        }
+    }
+}
