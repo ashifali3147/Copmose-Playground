f855de44da10f6439b8cbff07fb051a395ebf5b0
Ashif Ali <ashifali3147@gmail.com>
2026-07-11 09:48:16 +0530

    remove SignUpViewModel from SmartDashboardActivity

--- app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
+++ app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.tlw.composeplayground.projects.smartdashboard.ui.screen.SignUpScreen
 import com.tlw.composeplayground.projects.smartdashboard.ui.theme.ComposePlaygroundTheme
-import com.tlw.composeplayground.projects.smartdashboard.viewmodel.SignUpViewModel
 
 class SmartDashboardActivity : ComponentActivity() {
-    private val viewModel by viewModels<SignUpViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
