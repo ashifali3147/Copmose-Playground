d1404f8d0348057183a9bff06b0c00c88fd30b79
Ashif Ali <ashifali3147@gmail.com>
2026-07-17 16:03:05 +0530

    feat: NavDisplay added for navigation3 routes

--- app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
+++ app/src/main/java/com/tlw/composeplayground/projects/smartdashboard/SmartDashboardActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.tlw.composeplayground.projects.smartdashboard.ui.navigation.Routes
+import com.tlw.composeplayground.projects.smartdashboard.ui.screen.ControlDeviceScreen
 import com.tlw.composeplayground.projects.smartdashboard.ui.screen.SignUpScreen
 import com.tlw.composeplayground.projects.smartdashboard.ui.theme.ComposePlaygroundTheme
 
@@ -15,11 +23,36 @@ class SmartDashboardActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val backStack = rememberNavBackStack(Routes.SignUpPage)
             ComposePlaygroundTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                ) { innerPadding ->
-                    SignUpScreen(innerPadding)
+
+                    ) { innerPadding ->
+                    NavDisplay(
+                        backStack = backStack,
+                        transitionSpec = {
+                            slideInHorizontally { it } togetherWith
+                                    slideOutHorizontally { -it }
+                        },
+                        entryProvider = { key ->
+                            when (key) {
+                                is Routes.SignUpPage -> {
+                                    NavEntry(key) {
+                                        SignUpScreen(innerPadding, backStack)
+                                    }
+                                }
+
+                                is Routes.DashBoardScreen -> {
+                                    NavEntry(key) {
+                                        ControlDeviceScreen(innerPadding, backStack)
+                                    }
+                                }
+
+                                else -> error("Path not find")
+                            }
+                        }
+                    )
                 }
             }
         }
